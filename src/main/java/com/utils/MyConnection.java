c75a47b484cdf4ed13d00fac284ef4e554f3f6ee
Brush <26982912@qq.com>
2023-10-28 14:15:12 +0800

    1

--- src/main/java/com/utils/MyConnection.java
+++ src/main/java/com/utils/MyConnection.java
@@ -1,28 +1,34 @@
 package com.utils;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MyConnection{
 
-    private static final String DB_SEAWEED_URL = "jdbc:mysql://127.0.0.1:3306/HOUSE_OWNER_RECORD?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&transformedBitIsBoolean=true";
+//    private static String DB_SEAWEED_URL = "jdbc:mysql://127.0.0.1:3306/HOUSE_OWNER_RECORD?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&transformedBitIsBoolean=true";
     private static Statement statement = null;
     private static Connection connection = null;
-    static{
+
+
+    public static Statement getStatement(String url,String user,String password){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(DB_SEAWEED_URL,"root","dgsoft");
+            connection = DriverManager.getConnection(url,user,password);
             statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
             System.out.println("seaweedConnection successful");
+            return statement;
         }catch (Exception e){
             System.out.println("seaweedConnection is errer");
             e.printStackTrace();
+            return statement;
         }
     }
-    public static Statement getStatement(){
-        return statement;
-    }
+    public static void closeConnection() throws SQLException {
+        if(statement !=null){
+            statement.close();
+        }
+        if (connection!=null){
+            connection.close();
+        }
 
+    }
 }
